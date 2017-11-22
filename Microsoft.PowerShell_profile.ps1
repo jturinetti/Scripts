08058f61c5748f1d0bbc1019d4fe758dc3c6d789
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2017-11-21 21:43:17 -0600

    added script to obliterate chocolatey from a machine; cleaned up PS profile to use git ssh and automatically refresh PATH vars

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,8 @@
 # Assumes PowerShell 5+
 # Installs are performed globally (i.e. for all users)
 
+$shouldRefresh = $false
+
 # install git tools if not installed
 if (-Not (Get-Command "git" -ErrorAction SilentlyContinue)) {
     # install Chocolatey just to install git for Windows
@@ -8,21 +10,19 @@ if (-Not (Get-Command "git" -ErrorAction SilentlyContinue)) {
         Set-ExecutionPolicy Bypass -Scope Process -Force
         Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     }
-    choco install git -y
+    choco install git -y -params "/GitAndUnixToolsOnPath"
+    $shouldRefresh = $true
 }
 
 # install and import Posh-Git if not installed
 if (-Not (Get-Module -ListAvailable -Name Posh-Git)) {
-    Install-Module Posh-Git -Force    
+    Install-Module Posh-Git -Force
 }
 
 Import-Module Posh-Git
 
-# install and import Posh-SSH if not installed
-if (-Not (Get-Module -ListAvailable -Name Posh-SSH)) {
-    Install-Module Posh-SSH -Force    
+if ($shouldRefresh) {
+    refreshenv
 }
-
-Import-Module Posh-SSH
 
 # NOTE: Chocolatey will probably automatically edit this profile file somewhere down here after it installs.
