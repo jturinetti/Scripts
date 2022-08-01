7853a9901ffb9f00507d5a7a77cace44a1e95431
Jeremy Turinetti <jturinetti@users.noreply.github.com>
2022-07-31 22:35:10 -0500

    updating profile script again after some tweaking

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,21 @@
-# NOTE: Requires install of https://ohmyposh.dev/ beforehand.
-oh-my-posh init powershell --config "$env:POSH_THEMES_PATH\avit.omp.json" | Invoke-Expression
+# NOTE - Borrowed ideas from here: https://medium.com/@dorlugasigal/how-to-set-up-your-powershell-f004c90bbafb
 
-# NOTE: Other useful improvements: https://medium.com/@dorlugasigal/how-to-set-up-your-powershell-f004c90bbafb
+# Requires install of https://ohmyposh.dev/
+oh-my-posh init powershell --config "$env:POSH_THEMES_PATH\hotstick.minimal.omp.json" | Invoke-Expression
+
+# Requires install of PowerType
+# Enable-PowerType
+# Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
+# Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+# Set-PSReadLineKeyHandler -Key Tab -Function Complete -Debug
+
+# Requires install of Terminal-Icons
+Import-Module -Name Terminal-Icons
+
+# Requires install of PSFzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider ‘Ctrl+f’ -PSReadLineChordReverseHistory ‘Ctrl+r’
+
+# Requires install of thefuck
+$env:PYTHONIOENCODING="utf-8"
+iex "$(thefuck --alias)"
